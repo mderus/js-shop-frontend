6aff898678b5a23f748e886bc886bf75ecc9d41a
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-12 22:28:10 +0200

    display HomeScreen component, first reducer

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,39 @@
+import {useEffect} from 'react';
+import {Col, Row} from 'react-bootstrap';
+import {useDispatch, useSelector} from 'react-redux';
+import {listProducts} from '../actions/productActions';
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+
+const HomeScreen = () => {
+  const productList = useSelector((state) => state.productList);
+  const {loading, error, products} = productList;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
