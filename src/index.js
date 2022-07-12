9198cea845742a699c1109310083ed30494227b4
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-12 18:48:27 +0200

    installing packages and creating redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './bootstrap.min.css';
 import './index.css';
 import App from './App';
+import store from './store';
+import {Provider} from 'react-redux';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
