7ece4de4fcd56502c798e4c36d4b81d1d16200f0
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-14 23:49:16 +0200

    redux state for cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const App = () => {
   return (
@@ -13,8 +14,12 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart'>
+              <Route index element={<CartScreen />} />
+              <Route path=':id' element={<CartScreen />} />
+            </Route>
           </Routes>
         </Container>
       </main>
--- src/store.js
+++ src/store.js
@@ -9,10 +9,12 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
+import {cartReducer} from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
 const initialState = {};
