ebb867b0a0c22d67696f2cfa52dceb6d84da6f0e
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-15 00:10:19 +0200

    CartScreen logic

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -1,5 +1,28 @@
+import {useEffect} from 'react';
+import {Link, useLocation, useParams} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
+import {addToCart} from '../actions/cartActions';
+
 const CartScreen = () => {
-  return <div>CartScreen</div>;
+  const {id: productId} = useParams();
+
+  const {search} = useLocation();
+
+  const qty = search ? Number(search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const {cartItems} = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return <Row></Row>;
 };
 
 export default CartScreen;
