5b63e5d3e14f48eb4ee2675ece24ea06b04806f5
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-14 14:49:16 +0200

    product screen + redux product

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
+            <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/' element={<HomeScreen />} />
           </Routes>
         </Container>
