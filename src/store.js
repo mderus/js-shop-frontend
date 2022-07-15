fe999826237d73799911bbc3e029671c8636d26a
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-15 18:33:47 +0200

    redux config - user details

--- src/store.js
+++ src/store.js
@@ -10,7 +10,11 @@ import {
   productListReducer,
 } from './reducers/productReducers';
 import {cartReducer} from './reducers/cartReducers';
-import {userLoginReducer, userRegisterReducer} from './reducers/userReducers';
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+} from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -18,6 +22,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
